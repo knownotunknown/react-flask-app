6c826677d2167c68dc6dfbda514f99e9d3559a3c
knownotunknown <78577376+knownotunknown@users.noreply.github.com>
2023-10-22 11:19:39 -0500

    test

--- src/App.js
+++ src/App.js
@@ -2,70 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+//import './page.css';
+import Home from './page';
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(null);
-  const [file, setFile] = useState(null);
-
-  const handleFileChange = async (e) => {
-    console.log('this is the file');
-    const file = e.target.files[0];
-    if (file) {
-        setFile(file);
-
-        const formData = new FormData();
-        formData.append('file', file);
-
-        try {
-          fetch('http://localhost:5000/api/time').then(res => res.json()).then(data => {
-            setCurrentTime(data.time);
-          });
-            // Process the extractedText and set benefits
-        } catch (error) {
-            console.error("Error uploading and parsing the file:", error);
-        }
-    }
-};
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <BrowserRouter>
-          <div>
-            <Link className="App-link" to="/">Home</Link>
-            &nbsp;|&nbsp;
-            <Link className="App-link" to="/page2">Page2</Link>
-          </div>
-          <Switch>
-            <Route exact path="/">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                  Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <input
-                    type="file"
-                    accept=".pdf"
-                    onChange={handleFileChange}
-                    className="uploadButton"
-                />
-                <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Learn React
-                </a>
-                <p>The current time is {currentTime}.</p>
-            </Route>
-            <Route path="/page2">
-                <p>This is page 2!</p>
-            </Route>
-          </Switch>
-        </BrowserRouter>
-      </header>
-    </div>
-  );
+  return < Home/>
 }
 
 export default App;
--- src/page.js
+++ src/page.js
@@ -1,37 +1,61 @@
 "use client"
 import React, { useState } from 'react';
 import styles from './page.module.css';
+import axios from 'axios';
 import { ResultsPopup } from './benefit-results';
 
 export default function Home() {
     const [file, setFile] = useState(null);
     const [benefits, setBenefits] = useState([]);
 
-    const handleFileChange = (e) => {
+    const handleFileChange = async (e) => {
+        console.log('this is the file');
         const file = e.target.files[0];
         if (file) {
+            
             setFile(file);
+
+            const formData = new FormData();
+            formData.append('file', file);
+
+            try {
+                const response = await axios.post('/api/parse', formData, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                });
+                
+
+                console.log('this is the response', response)
+
+
+                // Now you can process the extractedText further...
+                // For demonstration, let's assume you set it directly to benefits
+                // setBenefits([extractedText]);
+            } catch (error) {
+                console.error("Error uploading and parsing the file:", error);
+            }
         }
     };
 
     return (
-        <>
-        <div className={styles.container}>
+        <><div className={styles.container}>
             <h1 className={styles.header}>PDF Benefits Analyzer</h1>
             <h2 className={styles.subHeader}>Upload a copy of your employment agreement and discover your benefits within seconds!</h2>
 
             <input
                 type="file"
                 accept=".pdf"
-                onChange={handleFileChange}
+                onChange={(e) => {handleFileChange(e)}}
                 className={styles.uploadButton}
             />
-
+            
             <div className={styles.resultsSection}>
                 {benefits.map((benefit, index) => (
                     <p key={index}>{benefit}</p>
                 ))}
             </div>
+
         </div>
         <ResultsPopup /></>
     );
